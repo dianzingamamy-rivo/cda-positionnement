935ed655710dc50a6bf469242b8e32254b6cdae9
dianzingamamy-rivo <shimumass43@gmail.com>
2026-09-16 17:08:32 +0200

    New Update

--- exercices/algo/algorithmes.js
+++ exercices/algo/algorithmes.js
@@ -6,20 +6,20 @@ console.log(" ");
 // Construction d'une fonction
 // Fonction
 function fizzBuzz(n){
-
-	if ((n%3)==0){
-		console.log("Fizz");
+	for (let i=1; i<=n; i++){
+		if ((i%3)==0 && (i%5)==0){
+			console.log("FizzBuzz");
+		}
+		else if ((i%3)==0){
+			console.log("Fizz");
+		}
+		else if ((i%5)==0){
+			console.log("Buzz");
+		}
+		else {
+			console.log(`${i}`);
+		}
 	}
-	else if ((n%5)==0){
-		console.log("Buzz");
-	}
-	else if ((n%3)==0 && (n%5)==0){
-		console.log("FizzBuzz");
-	}
-	else {
-		console.log(n);
-	}
-
 }
 
 // Test de la fonction
